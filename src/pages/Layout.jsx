6079b43b6890fd60026a9055d2fab49ed1a1100f
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-04-09 16:37:26 +0530

    added a siderbar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,26 +1,51 @@
 import { Container } from '@mui/material'
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography'
 import { makeStyles } from '@mui/styles'
 
+import { AppBar } from '@mui/material'
+
 function Layout(props) {
+    const drawerWidth = 240;
     const useStyles = makeStyles({
+        root: {
+            display: 'flex',
+        },
         page: {
             background: "#f9f9f9",
-            width: "100%"
+            width: "100%",
+        },
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerPaper: {
+            width: drawerWidth,
         }
     })
     const classes = useStyles()
     return (
-        <>
-        {/* app bar */}
-        
-        {/* sidebar */}
-        
-            <Container>
-                <div className={classes.page}>
-                    {props.children}
+        <div className={`first ${classes.root}`}>
+            {/* app bar */}
+
+            {/* side drawer */}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent" // variant prop is required for seeing the drawer    
+                anchor='left' // anchor prop is required for positioning the drawer  // left, right, top, bottom 
+                classes={{ // override the default styles of the drawer
+                    paper: classes.drawerPaper
+                }}
+            >
+                <div>
+                    <Typography color='secondary' variant='h5'>
+                        Side Drawer
+                    </Typography>
                 </div>
-            </Container>
-        </>
+            </Drawer>
+            <div className={classes.page}>
+                {props.children}
+            </div>
+        </div>
     )
 }
 
