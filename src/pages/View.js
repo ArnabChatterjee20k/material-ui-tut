1f7db6e1c34798774b17adf0cc5d34a516c22794
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-04-11 19:44:33 +0530

    changed the css grid to masonry

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 
 import { Container } from "@mui/material";
-import Grid from "@mui/material/Grid";
+// import Grid from "@mui/material/Grid";
+import Masonry from "react-masonry-css";
 import Notes_card from "./notes_card";
 export default function View() {
   const [notes, setNotes] = useState([]);
@@ -20,15 +21,27 @@ export default function View() {
     const new_note = await notes.filter(note=>note.id!=id)
     setNotes(new_note)
   }
+
+  // Breakpoints for the Masonry layout
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
   return (
     <Container sx={{padding:10}} className="view">
-      <Grid container spacing={3}>
-        {notes.map(note => (
-          <Grid item key={note.id} sm={12} xs={6} md={6} lg={4}>
-            <Notes_card note={note} handle_delete={handle_delete}/>
-          </Grid>
-        ))}
-      </Grid>
+        <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+        >
+          {notes.map(note => (
+            <div key={note.id} sm={12} xs={6} md={6} lg={4}>
+              <Notes_card note={note} handle_delete={handle_delete}/>
+            </div>
+          ))}
+        </Masonry>
     </Container>
   )
 }
